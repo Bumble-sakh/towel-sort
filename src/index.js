a834b56dda0e68a088b1f7f350224b7ae826d48e
Bumble <bumble.sakh@gmail.com>
2022-01-21 21:44:24 +1100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let result = [];
+    if (!matrix) return result;
+    let reverse = false;
+    matrix.forEach((row) => {
+        result = reverse ? result.concat(row.reverse()) : result.concat(row);
+        reverse = !reverse;
+    });
+    return result;
+};
